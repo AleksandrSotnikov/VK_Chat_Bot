1e831dc91ef10b856960e358a47a6b6b8fa3a74f
Aleksandr Sotnikov <strelok11211@gmail.com>
2020-08-08 17:40:39 +0600

    add class Screenshot

---
+++ src/main/java/ru/sotnikov/test/Screenshot.java
@@ -0,0 +1,14 @@
+package ru.sotnikov.test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Screenshot {
+    public static void main(String[] args) throws IOException, AWTException {
+        BufferedImage image = new Robot().createScreenCapture(new Rectangle(new Point(1,1),new Dimension(1,1)));
+        ImageIO.write(image, "png", new File("screenshot.png"));
+    }
+}
