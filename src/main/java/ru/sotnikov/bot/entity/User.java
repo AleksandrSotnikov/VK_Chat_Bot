05521dc99aa5372346c2c2a1c13e785fe1e0c392
Aleksandr Sotnikov <strelok11211@gmail.com>
2020-08-12 11:45:12 +0600

    User.java add get/set/constructor

--- src/main/java/ru/sotnikov/bot/entity/User.java
+++ src/main/java/ru/sotnikov/bot/entity/User.java
@@ -5,4 +5,46 @@ public class User {
     private static String lastName;
     private static String nickName;
     private static String id;
+
+    public User() {
+    }
+
+    public User(String firstName,String lastName,String nickName,String id) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setNickName(nickName);
+        setId(id);
+    }
+
+    public static String getFirstName() {
+        return firstName;
+    }
+
+    public static void setFirstName(String firstName) {
+        User.firstName = firstName;
+    }
+
+    public static String getLastName() {
+        return lastName;
+    }
+
+    public static void setLastName(String lastName) {
+        User.lastName = lastName;
+    }
+
+    public static String getNickName() {
+        return nickName;
+    }
+
+    public static void setNickName(String nickName) {
+        User.nickName = nickName;
+    }
+
+    public static String getId() {
+        return id;
+    }
+
+    public static void setId(String id) {
+        User.id = id;
+    }
 }
